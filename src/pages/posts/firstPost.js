d2907404f2640b27680045baaa7f89198e925bc1
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-09-29 23:52:09 +0900

    メタデータの追記

--- src/pages/posts/firstPost.js
+++ src/pages/posts/firstPost.js
@@ -1,9 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function firstPost() {
   return (
     <div>
-      <h1>最初の投稿</h1>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
       <Link href={"/"}>ホームへ戻る</Link>
     </div>
   );
